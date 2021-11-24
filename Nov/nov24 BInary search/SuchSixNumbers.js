7e25cbd03f81ff782f56c42211b08bbe693a40ce
Ramdas Hedgapure <hedgapurer@gmail.com>
2021-11-25 01:24:56 +0530

    such 6 num

--- Nov/nov24 BInary search/SuchSixNumbers.js
+++ Nov/nov24 BInary search/SuchSixNumbers.js
@@ -2,17 +2,73 @@ function runProgram(input) {
     var input = input.trim().split('\n')
     var n = +input[0].trim()
     let arr = []
-    for(let i=1; i<=n; i++){
+
+  for(let i=1; i<=n; i++){
         arr.push(+input[i].trim())
     }
-    
-    let lhs = (a * b) + c;
-    let rhs = (f+ e) * d;
 
+    var lhs = []
     for(let j=0; j<n; j++){
-        
+        for(let k=0; k<n; k++){
+          for(let l=0; l<n; l++){
+            lhs.push((arr[j] * arr[k])+arr[l])
+          }
+        }
     }
-   
+
+    var rhs =[]
+    for(let j=0; j<n; j++){
+        for(let k=0; k<n; k++){
+          for(let l=0; l<n; l++){
+            if(arr[l]!==0){
+              rhs.push((arr[j] + arr[k]) * arr[l])
+            }
+          }
+        }
+  }
+
+  rhs = rhs.sort((a,b)=>a-b)
+  lhs = lhs.sort((a,b)=>a-b)
+    console.log(lhs, rhs) 
+
+     count = 0;
+  //  for(var x=0; x<lhs.length; x++){
+  //    for(var y=0; y<rhs.length; y++){
+  //      if(rhs[y] == lhs[x]){
+  //        count++
+  //        break;
+  //      } 
+  //    }
+  //  }
+
+  // if(lhs.length > rhs.length){
+  //   temp = rhs;
+  //   rhs = lhs;
+  //   lhs = temp
+
+  // }
+  
+  for(var x=0; x<lhs.length; x++){
+    var l= 0;
+    var h = rhs.length
+
+    while(l<=h){
+
+      var mid =Math.floor(l +(h-l)/2);
+      if(rhs[mid] == lhs[x] ){
+        console.log('mid',rhs[mid], lhs[x] )
+        count ++
+      }
+      if(rhs[mid] > lhs[x]){
+        h = mid-1
+      } else l = mid+1
+
+     
+    }
+    
+  }
+
+   console.log(count)
 }
   
 if (process.env.USERNAME === "hedga") {
