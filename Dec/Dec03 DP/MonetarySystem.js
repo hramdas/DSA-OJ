c929fa44096d141f829123fdb7b4e05ea8d8a7e1
Ramdas Hedgapure <hedgapurer@gmail.com>
2021-12-08 12:01:23 +0530

    DP contest

--- Dec/Dec03 DP/MonetarySystem.js
+++ Dec/Dec03 DP/MonetarySystem.js
@@ -1,13 +1,25 @@
+function coins(num){
+   if(num <=5) {
+       return num
+    } else {val = (coins(num/2) + coins(num/3) + coins(num/4))
+       return Math.max(val, num)
+    }
+
+
+}
 function runProgram(input) {
     input = input.trim().split('\n').map(Number)
     var n = input.length;
-    console.log(input, n)
-   
+    // console.log(n)
+
+    for(let i=0; i<n; i++){
+        console.log(coins(input[i]))
+    }
 }
   
 if (process.env.USERNAME === "hedga") {
-    runProgram(`12
-    2`);
+    runProgram(`12 
+    2 `);
   } else {
     process.stdin.resume();
     process.stdin.setEncoding("ascii");
